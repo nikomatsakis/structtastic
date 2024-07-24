fc39ee3430cfeffcf463434edd714b7551883af3
Evan Schwartz <3262610+emschwartz@users.noreply.github.com>
2024-07-24 11:47:38 -0400

    Value doesn't need to be wrapped in an Rc

--- examples/non_send_future.rs
+++ examples/non_send_future.rs
@@ -1,13 +1,13 @@
 #![feature(async_closure)]
 
-use std::{cell::RefCell, rc::Rc};
+use std::cell::RefCell;
 
 // We are using the current_thread runtime because this version
 // of the macro returns a non-Send Future.
 #[tokio::main(flavor = "current_thread")]
 pub async fn main() {
     // This value is not thread-safe
-    let value = Rc::new(RefCell::new(22));
+    let value = RefCell::new(22);
 
     moro::scope_local(async |scope| {
         scope.spawn(async {
